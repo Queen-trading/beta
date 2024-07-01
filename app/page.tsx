d7d3ca0739935f8a579354b9646a41db5d942300
QueenDEV <queendev2022@gmail.com>
2024-07-01 16:10:51 -0500

    Update timeline

--- app/page.tsx
+++ app/page.tsx
@@ -2,42 +2,60 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24 gap-10">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm">
+    <main className="flex min-h-screen flex-col items-center p-24 gap-1">
+      <div className="z-0 w-full max-w-5xl items-center justify-between font-mono text-sm">
         <div className="flex h-52 w-full items-end justify-center">
           <Image
             src="/beta.png"
             alt="Beta Logo"
             width={300}
             height={300}
-            priority
+            
           />
         </div>
       </div>
 
-      <div className=" text-center font-semibold">
-      <p className=" text-white"> NO RUGS. NO GAMES. NO BULLSHIT.</p>
-      <p className=" text-white"> ALL COMMUNITY EFFORT.</p>
-      <p className=" text-white"> THIS IS JUST THE BEGINNING.</p>
-      <br></br>
-      <p className=" text-white"> Renounced Mint Authority.</p>
-      <p className=" text-white"> Contract Renounced. </p>
-      <p className=" text-white"> Liquidity Burned.  </p>
-      <p className=" text-white"> No Transaction Fees. No Tax.</p>
-      <br></br>
-      <p className=" text-white">Beta Token, essential to the Alpha ecosystem, translates activity into
-                                value. </p>
-                                
-      <p className=" text-white">It initiates our journey to reshape finance and ensure fair wealth
-                                  distribution.</p>
-      <p className=" text-white">Our perpetual, immutable system highlights DeFi's
-                                potential and rewards devoted members</p>
+      <div className="text-center">
+        <h1 className="text-9xl font-bold text-white">
+          Beta
+        </h1>
+      </div>
+      
+      <div className=" z-20 text-center beta-container">
+        <p className="text-4xl font-bold text-white">
+          Beta Token Post-Launch
+        </p>
+        <div className="beta-middle">
+          <Image 
+          className="beta-image"
+          src="/timeline.png" 
+          alt="timeline"
+          width={940}
+          height={1050}
+          priority
+          />
+        </div>
+      </div>
+
+      <div className=" text-center font-semibold text-white">
+      <p> NO RUGS. NO GAMES. NO BULLSHIT.</p>
+      <p> ALL COMMUNITY EFFORT.</p>
+      <p> THIS IS JUST THE BEGINNING.</p>
+      <br/>
+      <p> Renounced Mint Authority.</p>
+      <p> Contract Renounced. </p>
+      <p> Liquidity Burned.  </p>
+      <p> No Transaction Fees. No Tax.</p>
+      <br/>
+      <p>Beta Token, essential to the Alpha ecosystem, translates activity into value. </p>                          
+      <p>It initiates our journey to reshape finance and ensure fair wealth distribution.</p>
+      <p>Our perpetual, immutable system highlights DeFi's potential and rewards devoted members</p>
       </div>
 
       <div className=" flex flex-col items-center gap-6 pt-10">
         <div className=" flex flex-col items-center text-center lg:mb-0 bg-green-500 rounded-lg">
           <p className="text-2xl font-semibold px-[20px] py-3 text-white">
-          0xca530408c3e552b020a2300debc7bd18820fb42f
+              0xca530408c3e552b020a2300debc7bd18820fb42f
           </p>
         </div>
 
